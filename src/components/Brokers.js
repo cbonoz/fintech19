10f9c6781ee8e739667bb0864c2308204eb0e2a6
cbonoz <chrisdistrict@gmail.com>
2019-02-24 10:41:36 -0500

    add broker chart

--- src/components/Brokers.js
+++ src/components/Brokers.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { assignColor, assignNormalizedColor } from '../helper';
-import data from '../data';
+import { Modal, Button } from 'react-bootstrap'
+import ReactChartkick, { BarChart, ColumnChart } from 'react-chartkick'
+import Chart from 'chart.js'
+import data, { MAX_BROKER, MIN_BROKER } from '../data';
+
+ReactChartkick.addAdapter(Chart)
 
 
 class Brokers extends Component {
+
+
+    state = {
+        show: false,
+        currentBroker: {}
+    }
+
+    constructor(props) {
+        super(props)
+        this.handleClose = this.handleClose.bind(this)
+    }
 
     componentDidMount() {
         let { brokers, header } = this.props
@@ -12,8 +28,19 @@ class Brokers extends Component {
         console.log(data.MIN_BROKER, data.MAX_BROKER)
     }
 
+    handleClose() {
+        console.log('handleClose')
+        this.setState({ show: false });
+    }
+
+    handleShow() {
+        console.log('show')
+        this.setState({ show: true });
+    }
+
     render() {
         let { brokers, header } = this.props
+        const { currentBroker, show } = this.state
         brokers = brokers.map(b => {
             b.color = assignNormalizedColor(b.score)
             return b
@@ -28,7 +55,7 @@ class Brokers extends Component {
                 text = 'Broker Score'
             }
 
-            return{
+            return {
                 dataField: h,
                 sort: true,
                 text
@@ -41,11 +68,18 @@ class Brokers extends Component {
             }
         }
 
+        const rowEvents = {
+            onClick: (e, row, rowIndex) => {
+                console.log('clicked', row)
+                this.setState({ currentBroker: row, show: true })
+            }
+        }
+
         return (
             <div className='data-row'>
-            <p className="data-table-description">
-                This table shows the relative performance of brokers being attached to profit-generating submissions that were successfully bound. 
-                A higher score indicates the broker is associated with a higher number of profitable policy transactions, while a lower number indicates the broker is underperforming or more associated with lower profitability quotes.
+                <p className="data-table-description">
+                    This table shows the relative performance of brokers being attached to profit-generating submissions that were successfully bound.
+                    A higher score indicates the broker is associated with a higher number of profitable policy transactions, while a lower number indicates the broker is underperforming or more associated with lower profitability quotes.
             </p>
 
                 <BootstrapTable
@@ -53,7 +87,34 @@ class Brokers extends Component {
                     data={brokers}
                     columns={columns}
                     rowStyle={rowStyle}
+                    rowEvents={rowEvents}
                 />
+
+
+                <Modal
+                    size="lg"
+                    show={show}
+                    onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Relative Broker Performance</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h5><b>Broker ID: </b>{currentBroker['x']}</h5>
+                        <div className='chart-section'>
+                            <ColumnChart data={[["Minimum", MIN_BROKER], [currentBroker['x'], currentBroker['y']], ["Maximum", MAX_BROKER]]} />
+                            {/* <ul>
+                                <li>Minimum</li>
+                            </ul> */}
+                            <br /><br />
+                            <p><b>BrokerScore: </b>{currentBroker['score']}</p>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Cancel
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
             </div>
         );
